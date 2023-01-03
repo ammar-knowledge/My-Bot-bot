a146575977506af7c7a034d101d8fc9c82881c8d
Ammar Ahmed Butt <ammar.ahmed@devsinc.com>
2023-01-03 07:26:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,6 @@
 const commitlint = require("./lib/lint");
 const EventSource = require('eventsource');
 
-// install with: npm install @octokit/webhooks
-const { Webhooks, createNodeMiddleware } = require("@octokit/webhooks");
-const webhooks = new Webhooks({
-  secret: "mysecret",
-});
-
-webhooks.onAny(({ id, name, payload }) => {
-  console.log(name, "event received");
-});
-
-require("http").createServer(createNodeMiddleware(webhooks)).listen(3000);
-// can now receive webhook events at /api/github/webhooks\
-
-const { token } = await app.oauth.createToken({
-  async onVerification(verification) {
-    await sendMessageToUser(
-      request.body.phoneNumber,
-      `Your code is ${verification.user_code}. Enter it at ${verification.verification_uri}`
-    );
-  },
-});
-
 module.exports = robot => {
   // For more information on building apps:
   // https://probot.github.io/docs/
