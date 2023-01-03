8a26b89b554cc43108e529619a583ab19eb77b2d
Ammar Ahmed Butt <ammar.ahmed@devsinc.com>
2023-01-03 07:17:01 +0500

    dd

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // Ours
 const commitlint = require("./lib/lint");
+const EventSource = require('eventsource');
+
+// install with: npm install @octokit/webhooks
+const { Webhooks, createNodeMiddleware } = require("@octokit/webhooks");
+const webhooks = new Webhooks({
+  secret: "mysecret",
+});
+
+webhooks.onAny(({ id, name, payload }) => {
+  console.log(name, "event received");
+});
+
+require("http").createServer(createNodeMiddleware(webhooks)).listen(3000);
+// can now receive webhook events at /api/github/webhooks
 
 module.exports = robot => {
   // For more information on building apps:
